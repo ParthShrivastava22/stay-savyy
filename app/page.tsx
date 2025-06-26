3e854b50469e0ce31b3260329de8e2401eec331b
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-06-26 10:27:45 +0530

    Change HomeProps to make searchParams a Promise in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,25 @@ import { getHomes } from "@/actions/getHomes";
 import HomeList from "@/components/home/HomeList";
 
 interface HomeProps {
-  searchParams: {
+  searchParams: Promise<{
     title: string;
     country: string;
     state: string;
     city: string;
-  };
+  }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
   const awaitedParams = await searchParams;
   const homes = await getHomes(awaitedParams);
   if (!homes) return <div>No homes found...</div>;
+  const homesData = homes.map((home) => ({
+    ...home,
+    availableTo: home.availableTo ?? undefined,
+  }));
   return (
     <div>
-      <HomeList homes={homes} />
+      <HomeList homes={homesData} />
     </div>
   );
 }
